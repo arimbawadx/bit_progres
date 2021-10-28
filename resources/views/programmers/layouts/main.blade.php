1ea758225d0a8c27265abeec4f6b5f99f57160e9
arimbawadx <yogade9595.yd@gmail.com>
2021-10-28 18:32:14 +0800

    new change

--- resources/views/programmers/layouts/main.blade.php
+++ resources/views/programmers/layouts/main.blade.php
@@ -14,7 +14,8 @@
   <!-- Theme style -->
   <link rel="stylesheet" href="{{ asset('lte/dist/css/adminlte.min.css') }}">
   <!-- datatables -->
-  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.24/css/dataTables.bootstrap4.min.css">
+  <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.2/css/bootstrap.css">
+  <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.3/css/dataTables.bootstrap4.min.css">
   <link rel="icon" type="image/png" href="{{asset('lte/dist/img/bit.png')}}">
   
 </head>
@@ -112,7 +113,9 @@
   <!-- AdminLTE App -->
   <script src="{{ asset('lte/dist/js/adminlte.js') }}"></script>
   <!-- datatables -->
-  <script src="https://cdn.datatables.net/1.10.22/js/jquery.dataTables.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
+  <script src="https://cdn.datatables.net/1.11.3/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.11.3/js/dataTables.bootstrap4.min.js"></script>
   <script type="text/javascript">
     $(document).ready( function () {
       $('#datatables').DataTable();
